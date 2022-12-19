d079cbaa100699b84dbbab714a341459714a6c6a
caelifer09 <c.baeza9@gmail.com>
2022-12-19 10:44:15 -0300

    Subido el acerca de

---
+++ src/components/Acerde.jsx
@@ -0,0 +1,18 @@
+import '../styles/styles.css'
+
+const AcercaDe = () => {
+  return (
+    <div className="contenedor">
+        <p className="parrafo">
+            Dev: Claudio Baeza</p>
+            <p className="negrita">Tecnologias utilizadas:</p>
+        <p>Reactjs@vite + bootstrap</p>
+        <p>JSON</p>
+        <p>TheCocktailDB</p>
+        <p>Agradecimiento:</p>
+        <p>Juan Pablo de la torre Valdez y su curso de udemy</p>
+    </div>
+  )
+}
+
+export default AcercaDe
